7ba79b9284c84af22fcfeb0dab0263459f90601a
JL-er <997529190@qq.com>
2023-10-20 17:47:26 +0800

    1

--- test.py
+++ test.py
@@ -11,18 +11,19 @@ wkv5_cuda = load(name="wkv5", sources=["cuda/wkv5_op_state.cpp", f"cuda/wkv5_cud
 
 B = 2
 T = 3
-C = 4
-H = 5
+C = 128
+H = 2
 
 # 创建测试输入张量
-r = torch.randn(B, T, C, H, dtype=torch.float32)
-k = torch.randn(B, T, C, H, dtype=torch.float32)
-v = torch.randn(B, T, C, H, dtype=torch.float32)
-w = torch.randn(H, dtype=torch.float32)
-u = torch.randn(B, T, C, H, dtype=torch.float32)
-
+r = torch.randn(B, T, C, device='cuda', dtype=torch.bfloat16).contiguous()
+k = torch.randn(B, T, C,device='cuda', dtype=torch.bfloat16).contiguous()
+v = torch.randn(B, T, C,device='cuda', dtype=torch.bfloat16).contiguous()
+w = torch.randn(B, C,device='cuda', dtype=torch.float32).contiguous()
+u = torch.randn(B, C,device='cuda', dtype=torch.bfloat16).contiguous()
+y = torch.empty((B, T, C),device='cuda', dtype=torch.bfloat16).contiguous() # .uniform_(-1, 1)
+print(y)
 # 调用前向传播函数
-y = wkv5_cuda.forward(B, T, C, H, r, k, v, w, u)
+y = wkv5_cuda.forward(B, T, C, H, r, k, v, w, u, y)
 
 # 确保输出张量的形状和类型正确
 
